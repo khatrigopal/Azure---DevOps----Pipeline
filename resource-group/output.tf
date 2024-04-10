f16accb8d10d2afcf4afc5bc55e8ad4dcd042d66
Bharat Sharma <devopstigers2@gmail.com>
2024-04-10 06:05:04 +0000

    Updated output.tf

--- resource-group/output.tf
+++ resource-group/output.tf
@@ -1,3 +1,3 @@
 output "resource_group" {
-  value = { for c in var.resource_group : c.name => aws_lambda_function.lambda_functions[c.function_name] }
+  value = { for c in var.resource_group : c.name => azurerm_resource_group.resourcegroups[c.name] }
 }
