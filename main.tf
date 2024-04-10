237b9e2e7b3b938e119b588ce29f2aefb41f63b2
Bharat Sharma <devopstigers2@gmail.com>
2024-04-10 07:06:18 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -6,10 +6,18 @@ module "resource_group" {
     {
      name = "testingtiger"
      location = "WestUS"
+    },
+    {
+     name = "azuretiger"
+     location = "WestUS"
     }
   ]  
 }
 
 output "rg1" {
   value = module.resource_group.resource_group["testingtiger"].name
+}
+
+output "rg1" {
+  value = module.resource_group.resource_group["azuretiger"].name
 }
