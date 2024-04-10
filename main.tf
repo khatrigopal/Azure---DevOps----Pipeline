1c377c99909ad7c3c83d1058b080554ba97dc9f9
Bharat Sharma <devopstigers2@gmail.com>
2024-04-10 07:06:30 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,6 @@ output "rg1" {
   value = module.resource_group.resource_group["testingtiger"].name
 }
 
-output "rg1" {
+output "rg2" {
   value = module.resource_group.resource_group["azuretiger"].name
 }
