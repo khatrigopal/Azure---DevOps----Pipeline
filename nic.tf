853ce0e492ff7f045453b4ca8d5f2fe6eadb891d
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 08:43:42 +0000

    Updated nic.tf

--- nic.tf
+++ nic.tf
@@ -8,6 +8,7 @@ nic = {
     location            = "east us"
     resource_group_name = module.resource_group.resource_group["auzretiger-rg1"].name
     subnet_id = module.subnet.subnets["auzretiger-fronted-subnet"].id
+    public_ip_address_id = module.nic.nic["auzretiger-frontend-nic"].id
   }
     
   
