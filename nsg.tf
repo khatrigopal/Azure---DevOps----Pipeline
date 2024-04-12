add85e2d533be895f2abf040521ddbcc999bb183
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 12:03:20 +0000

    Updated nsg.tf

--- nsg.tf
+++ nsg.tf
@@ -2,7 +2,7 @@ module "nsg" {
 source = "./nsg"
 nsg = {
   "nsg1" = {
-    name                = "auzretiger-frontendvm-nsg"
+    name                = "auzretiger-frontend-nsg"
     location            = "east us"
     resource_group_name = module.resource_group.resource_group["auzretiger-rg1"].name
   }
