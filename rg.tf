c9394dd6059843fe86b4e83f4570be12510d3947
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 05:35:28 +0000

    Updated rg.tf

--- rg.tf
+++ rg.tf
@@ -5,17 +5,17 @@ module "resource_group" {
   rg = {
 
     "rg1" = {
-    {
+    
      name = "auzretiger-rg1"
      location = "east us"
-    }
+    
     },
     "rg2" = {
-    {
+    
      name = "auzretiger-rg2"
      location = "east us"
-    }
+    
     } 
-  }
+  } 
 }
 
