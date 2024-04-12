e14253c91d4f818e1e4ae85f57f163479c790a5d
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 08:05:48 +0000

    Updated main.tf

--- public-ip/main.tf
+++ public-ip/main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_public_ip" "public-ip" {
   for_each = { for p in var.publicip : p.name => p}  
   
-  name                = each.valune.name
+  name                = each.value.name
   resource_group_name = each.value.resource_group_name
   location            = each.value.location
   allocation_method   = "Static"
