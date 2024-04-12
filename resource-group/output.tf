b192a39dfe75d58ff0f09a56e176f6aae6a2b8bf
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 05:39:49 +0000

    Updated output.tf

--- resource-group/output.tf
+++ resource-group/output.tf
@@ -1,3 +1,3 @@
 output "resource_group" {
-  value = { for c in var.resource_group : c.name => azurerm_resource_group.resource_group[c.name] }
+  value = { for c in var.rg : c.name => azurerm_resource_group.resource_group[c.name] }
 }
