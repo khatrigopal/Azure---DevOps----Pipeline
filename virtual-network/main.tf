a02167eb9520976110b4899230f2f9e3d54ce2b3
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 04:14:05 +0000

    Updated main.tf

--- virtual-network/main.tf
+++ virtual-network/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_virtual_network" "example" {
   for_each        = { for c in var.vnet : c.name => c }
   name                = each.value.name
   location            = each.value.location
-  resource_group_name = module.resource_group.resource_group[*].name
+  #resource_group_name = module.resource_group.resource_group[*].name
   address_space       = each.value.address_space
 }
