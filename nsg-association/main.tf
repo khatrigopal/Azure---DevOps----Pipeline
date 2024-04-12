51215c43136e38cd9f9f9711cdbe530f664ff417
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 12:24:55 +0000

    Updated main.tf

--- nsg-association/main.tf
+++ nsg-association/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_interface_security_group_association" "association" {
-  for_each                  = { for as in var.nsg-map : as.name => as}
+  for_each                  = var.nsg-map
   network_interface_id      = each.value.network_interface_id
   network_security_group_id = each.value.network_security_group_id
 }
