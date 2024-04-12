33dd9296adc0e2cf65cd94e6cf1a369676180152
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 05:47:37 +0000

    Updated main.tf

--- resource-group/main.tf
+++ resource-group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "resource_group" {
-  for_each = var.rg
+  for_each = { for c in var.rg : c.name => c}
   name     = each.value.name
   location = each.value.location
 }
