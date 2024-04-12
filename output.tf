a29c43b1f15cb9891fbf74023589bfd85da00698
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 12:45:35 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -42,11 +42,11 @@ output "frontend_nic_id" {
   value = module.nic.nic["auzretiger-frontend-nic"].id
 }
 
-output "linux-vm1-id" {
-  value = module.vms.vms["azure-tieger-linux-vm1"].id
-}
+// output "linux-vm1-id" {
+//   value = module.vms.vms["azure-tieger-linux-vm1"].id
+// }
 
-output "linux-vm1-name" {
-  value = module.vms.vms["azure-tieger-linux-vm1"].name
-}
+// output "linux-vm1-name" {
+//   value = module.vms.vms["azure-tieger-linux-vm1"].name
+// }
 
