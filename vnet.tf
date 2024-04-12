e561804b635aff0d78ad8881fd6d74d2d7b03740
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 05:55:37 +0000

    Updated vnet.tf

--- vnet.tf
+++ vnet.tf
@@ -1,20 +1,4 @@
-// module "virtual-network" {
-//   source = "./virtual-network"
-
-
-// vnet = [
-//    {
-//     name                = "auzretiger-vnet1"
-//     location            = "east us"
-//     resource_group_name = module.resource_group.resource_group["auzretiger-rg1"].name
-//     address_space       = ["10.0.0.0/16"]
-//   },
-//   {
-//     name                = "auzretiger-vnet1"
-//     location            = "east us"
-//     resource_group_name = module.resource_group.resource_group["auzretiger-rg2"].name
-//     address_space       = ["20.0.0.0/16"]
-//   }
-// ]
-
-// }
+module "virtual-network" {
+  source = "./virtual-network"
+  var = var.net
+}
