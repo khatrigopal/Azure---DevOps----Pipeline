4f0c5e46e305ffe5ea0237c0cfe33eb20d12dcae
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 07:14:22 +0000

    Updated subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -5,17 +5,17 @@ subnet = {
   "subnet1" = {
    
     name                = "auzretiger-fronted-subnet"
-    location            = "east us"
+    resource_group_name = module.resource_group.resource_group["auzretiger-rg1"].name
     virtual_network_name = module.virtual-network.virtual-network["auzretiger-vnet1"].name
-    address_space       = ["10.0.1.0/24"]
+    address_prefixes       = ["10.0.1.0/24"]
   },
 
   "subnet2" = {
    
     name                = "auzretiger-backend-subnet"
-    location            = "east us"
+    resource_group_name = module.resource_group.resource_group["auzretiger-rg2"].name
     virtual_network_name = module.virtual-network.virtual-network["auzretiger-vnet2"].name
-    address_space       = ["20.0.1.0/24"]
+    address_prefixes       = ["20.0.1.0/24"]
   }
 }
 }
