3afcd57e08b3a4336bdffbab14f6ec94bd930395
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 12:28:29 +0000

    Updated linux_vms.tf

--- linux_vms.tf
+++ linux_vms.tf
@@ -1,23 +1,23 @@
-// module "vms" {
-//   source = "./virtual-machine"
+module "vms" {
+  source = "./virtual-machine"
 
-// vms = {
-//   "linux_vm1" = {
+vms = {
+  "linux_vm1" = {
    
-//     name                  = "azure-tieger-linux-vm1"
-//     resource_group_name   = module.resource_group.resource_group["auzretiger-rg1"].name
-//     location              = "east us"
-//     size                  = "Standard_DS1_v2"
-//     admin_username        = "devopsadmin"
-//     admin_password        = "P@ssw01rd@123"
-//     network_interface_ids = [module.nic.nic["auzretiger-frontend-nic"].id]
-//     source_image_reference = {
-//       publisher = "Canonical"
-//       offer     = "0001-com-ubuntu-server-focal"
-//       sku       = "20_04-lts"
-//       version   = "latest"
-//   }
-//   }
+    name                  = "azure-tieger-linux-vm1"
+    resource_group_name   = module.resource_group.resource_group["auzretiger-rg1"].name
+    location              = "east us"
+    size                  = "Standard_DS1_v2"
+    admin_username        = "devopsadmin"
+    admin_password        = "P@ssw01rd@123"
+    network_interface_ids = [module.nic.nic["auzretiger-frontend-nic"].id]
+    source_image_reference = {
+      publisher = "Canonical"
+      offer     = "0001-com-ubuntu-server-focal"
+      sku       = "20_04-lts"
+      version   = "latest"
+  }
+  }
   
-// }
-// }
+}
+}
