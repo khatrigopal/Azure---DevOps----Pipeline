9f14d6a671cc862508a37939b78a1bd0ac544334
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 04:19:10 +0000

    Added variables.tf

---
+++ virtual-network/variables.tf
@@ -0,0 +1,9 @@
+variable "vnet" {
+  type = list(object({
+    name                = each.value.name
+    location            = each.value.location
+    resource_group_name = each.value.resource_group_name
+    address_space       = each.value.address_space
+    
+  }))
+}
