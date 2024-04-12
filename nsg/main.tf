106fee2784421c5f9ea41f11ad42e62ef6e73314
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 12:02:52 +0000

    Updated main.tf

--- nsg/main.tf
+++ nsg/main.tf
@@ -3,4 +3,16 @@ resource "azurerm_network_security_group" "nsg" {
   name                = each.value.name
   location            = each.value.location
   resource_group_name = each.value.resource_group_name
+
+  security_rule {
+    name                       = "Allow_All"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
