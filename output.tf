190e218ebe4c87d57a2efcb869d0d4efe36e5576
Bharat Sharma <devopstigers2@gmail.com>
2024-04-12 05:56:57 +0000

    Updated output.tf

--- output.tf
+++ output.tf
@@ -6,10 +6,10 @@ output "rg2" {
   value = module.resource_group.resource_group["auzretiger-rg2"].name
 }
 
-// output "vnet1" {
-//   value = module.virtual-network.virtual-network["auzretiger-vnet1"].name
-// }
+output "vnet1" {
+  value = module.virtual-network.virtual-network["auzretiger-vnet1"].name
+}
 
-// output "vnet2" {
-//   value = module.virtual-network.virtual-network["auzretiger-vnet2"].name
-// }
+output "vnet2" {
+  value = module.virtual-network.virtual-network["auzretiger-vnet2"].name
+}
